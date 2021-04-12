4b17bd8b6bbe1f2765537a6a1fde3d920e389e2f
Davidbp99 <40523727+Davidbp99@users.noreply.github.com>
2021-04-12 22:35:22 +0200

    Added PlayerInitialSpawn hook and made whitelistgroup system

--- gwhitelist/lua/autorun/sh_whitelist.lua
+++ gwhitelist/lua/autorun/sh_whitelist.lua
@@ -16,11 +16,15 @@ gwhitelist.config = {} or gwhitelist.config
 include("autorun/config.lua") gwhitelist:Print("config.lua loaded") -- it loads the config file.
 
 hook.Add("CheckPassword", "whitelist", function(steamID64)
+  if (game.SinglePlayer() == false) then -- to block it to run on any singleplayer games
+  if (gwhitelist.config.activegroup == true) and (gwhitelist.config.active == true) then
+    return true,gwhitelist:Print("You got gwhitelist.config.activegroup set to true. That means we need to block the gwhitelist.config.active from running")
+  end
     if (gwhitelist.config.active == true) then
             local steamid = util.SteamIDFrom64(steamID64)
 
             if gwhitelist.config.blacklist[steamid] then -- It needs to check the user first if they are on the blacklist. If they are not then it will go down to normal whitelist check
-            	gwhitelist:Print("(" .. steamid .. ") is on the blacklist, kicking...")
+              gwhitelist:Print("(" .. steamid .. ") is on the blacklist, kicking...")
 
                 return false, gwhitelist.config.blacklistmessage
             end
@@ -33,10 +37,35 @@ hook.Add("CheckPassword", "whitelist", function(steamID64)
 
                 return false, gwhitelist.config.kickmessage
             else -- Just in case you did not mean to have someone on the whitelist. you can get their steamid this way.
-            	 
-            	return true, gwhitelist:Print("(" .. steamid .. ") is on the whitelist")
+               
+              return true, gwhitelist:Print("(" .. steamid .. ") is on the whitelist")
             end
         end
     
     end)
 
+hook.Add( "PlayerInitialSpawn", "whitelistgroup", function( ply )
+   if (game.SinglePlayer() == false) then -- to block it to run on any singleplayer games
+      if (gwhitelist.config.activegroup == true) then
+local steamid = ply:SteamID()
+
+ if gwhitelist.config.blacklist[steamid] then -- It needs to check the user first if they are on the blacklist. If they are not then it will go down to normal whitelist check
+              gwhitelist:Print("(" .. steamid .. ") is on the blacklist, kicking...")
+
+                ply:Kick(gwhitelist.config.blacklistmessage)
+            end-- We need to run the blacklist check first.
+
+
+ 
+
+if (gwhitelist.config.whitelistgroup[ply:GetUserGroup()] == true) then
+
+return true, gwhitelist:Print("(" .. ply:Nick() .. ") is on the whitelist Group: (" .. ply:GetUserGroup() .. ")")
+else if not gwhitelist.config.whitelist[steamid] then
+  gwhitelist:Print("(" .. ply:Nick() .. ") is not the whitelist. Group: (" .. ply:GetUserGroup() .. ")" )
+  ply:Kick(  )
+end
+
+end
+end
+end)
