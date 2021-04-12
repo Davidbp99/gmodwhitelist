e45d5f8e3d87f9e2ae003d9acf8f7d1c8ba97894
Davidbp99 <40523727+Davidbp99@users.noreply.github.com>
2021-04-12 20:31:38 +0200

    updated my wording.

--- gwhitelist/lua/autorun/config.lua
+++ gwhitelist/lua/autorun/config.lua
@@ -1,4 +1,4 @@
-// Made by STEAM_0:1:56187412 (David)
+--// Made by STEAM_0:1:56187412 (David)
 
 gwhitelist:Print("config for whitelist update loaded")
 
@@ -11,13 +11,14 @@ gwhitelist.config.blacklistmessage = "You are on a blacklist. You can get back o
 gwhitelist.config.welcomemessage = "Looks like you are whitelisted"
 
 gwhitelist.config.whitelist = { -- list who should be allowed to enter after they entered the server password or at all.
-["STEAM_0:0:0"] = true, //SteamID of any player on a singleplayer server. Keep this here if you dont want the whitelist system to block you from joining singleplayer
+["STEAM_0:0:0"] = true, --//SteamID of any player on a singleplayer server. Keep this here if you dont want the whitelist system to block you from joining singleplayer
 }
 
-gwhitelist.config.blacklist = { -- If you got someone you want to be sure you don't add by mistake to the whitelist. Then add their steamid below (just as above).
+gwhitelist.config.blacklist = { -- If you got someone that you want to make sure never gets added into the whitelist. Then you can put them below
+    -- ["Steamid"] = true, --// Is a special gmod player.
 
 
 }
 
-// If you add a users steamid to whitelist and blacklist then the user will be blocked to join.
-// As the blacklist is checked first before the whitelist
+--// If you add a users steamid to whitelist and blacklist then the user will be blocked to join.
+--// As the blacklist is checked first before the whitelist
